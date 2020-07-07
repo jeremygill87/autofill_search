f713dc5bf11ae0341d73a5bec9cd4d1b43c2b602
Ace Yogurt <jeremygill87@gmail.com>
2020-07-07 14:39:56 -0400

    created fetchresults function.  Not tested yet.

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./search.css";
+import axios from 'axios';
 
 class Search extends React.Component {
 
@@ -12,7 +13,45 @@ class Search extends React.Component {
             loading: false,
             message: ""
         };
+        this.cancel = '';
     }
+
+    //Fetch search results and update state
+    fetchSearchResults = (updatedPageNo = '', query) => {
+        
+        const pageNumber = updatedPageNo ? `&page=${updatedPageNo}`
+        :'';
+        
+        //Default search result limit is 20
+        const searchURL = `https://pixabay.com/api/?key=12413278-79b713c7e196c7a3defb5330e&q=${query}${pageNumber}`;
+        
+            if (this.cancel) {
+                this.cancel.cancel();
+            }
+            this.cancel = axios.CancelToken.source();
+            
+            axios
+                .get(searchURL, {
+                    cancelToken: this.cancel.token,
+                })
+                .then((res) => {
+                    const resultNotFoundMsg = !res.data.hits.length ? 'There are no more search results.  Please try a new search.': '';
+                    
+                    this.setState({
+                        results: res.data.hits,
+                        message: resultNotFoundMsg,
+                        loading: false,
+                    });
+                })
+                .catch((error) => {
+                    if (axios.isCancel(error) || error) {
+                        this.setState({
+                            loading: false,
+                            message: 'Failed to fetch results.  Please check network',
+                        });
+                    }
+                });
+            };
 
     handleInputChange = (event) => {
         const query = event.target.value;
